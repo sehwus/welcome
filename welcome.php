a3988b58775b88b46acf2b246a3ce559964106ce
Şehmus <82837008+sehwus@users.noreply.github.com>
2021-05-12 07:21:25 +0300

    Create welcome.php

---
+++ welcome.php
@@ -0,0 +1,22 @@
+#include <vector>
+#include <string>
+std::vector<std::string> Languages;
+
+int main()
+{
+	// User Info
+	std::string Name = "Şehmus";
+	int Age = 20;
+	std::string Country = "Turkey";
+	bool isStudent = true;
+	std::string Interests = ", learning coding ,teaching and sleeping.";
+	std::string FavoriteLanguage = "cpp";
+	const char* LanguagesSpoken[] = { "Turkish","English" };
+	void* PSize; //meme
+	// Languages
+
+	Languages.push_back("C#");
+	Languages.push_back("php");
+
+	return 0;
+}
